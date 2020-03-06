fc49502b2057cd18f35ff18336fe32a4affb2460
atran0802 <59665746+atran0802@users.noreply.github.com>
2020-03-06 11:22:35 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const path = require('path');
 const express = require('express');
-
 const ejs = require('ejs');
+const gallery = require('./gallery');
 
 /* Runs express */
 const app = express();
 
 /* Runs ejs */
 app.set('view engine', 'ejs');
+
+/* Get endpoint handlers to render */
+app.get('/',function(req, res) {  
+  res.render('index',{gallery});
+});
 
 /* Serving static files in express */
 app.use(express.static(path.join(__dirname, 'public')));
